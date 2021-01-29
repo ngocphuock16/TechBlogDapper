232d4ce7c23027bd5e5ab4dc826e7e39b6775353
ngocphuock16 <74942074+ngocphuock16@users.noreply.github.com>
2021-01-29 09:44:23 +0700

    Tech

---
+++ TechBlogWeb/TechBlogWeb.Web/Controllers/AboutController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TechBlogWeb.Domain.Response.About;
+
+namespace TechBlogWeb.Web.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<About> abouts = new List<About>();
+            abouts = Helper.ApiHelper<List<About>>.HttpGetAsync("api/about/gets");
+            return View(abouts);
+        }
+    }
+}
